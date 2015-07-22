9f2d6430f5c0a61fcffecee251e4946422cd528b
Danny Tran <danny@dqtran.com>
2015-07-22 02:12:37 -0600

    minor changes

--- server/controllers/DashboardController.js
+++ server/controllers/DashboardController.js
@@ -1,19 +1,4 @@
 var Promise = require('bluebird');
 
-module.exports = {
-
-    'index': function(req, res, next){
-    	res.view('dashboard/index', {
-      		title: 'dtran-design',
-    	});
-    },
-
-    'edit': function(req, res, next){
-    	res.view('dashboard/edit', {
-    		title: 'dtran-design'
-    	})
-    },
-
-    'update': function(req, res, next){
-    },    
+module.exports = {   
 };
--- server/routes/DashboardRouter.js
+++ server/routes/DashboardRouter.js
@@ -1,11 +1,8 @@
 module.exports = function (app, express) {
-
-
 	 var router = express.Router();
 	 var policies = [abe.policies.passport, abe.policies.basicAuth];
 
     router.all('*', policies);
 
 	router.get('/', abe.controllers.dashboard.index);
-	app.use('/dashboard', router);
 };
