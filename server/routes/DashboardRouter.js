8abdcbc24fa04a3b662f7f911387c4b4218c1634
Danny Tran <danny@dqtran.com>
2015-07-22 02:13:53 -0600

    delete dashboard

--- server/routes/DashboardRouter.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app, express) {
-	 var router = express.Router();
-	 var policies = [abe.policies.passport, abe.policies.basicAuth];
-
-    router.all('*', policies);
-
-	router.get('/', abe.controllers.dashboard.index);
-};
